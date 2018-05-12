c980a294d85a5ef291fc4088c3ca864465445536
Wes Mora <wesmora@gmail.com>
2018-05-12 13:57:51 -0600

    Homewor4

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+var random_results;
+var losts;
+var wins;
+
+for(var i = 0; i < 4; i++){
+    var random = Math.floor (Math.random() * 12)
+        console.log (random)
+    // var mariopowerupcontai = $("<div>") 
+    // mariopowerupcontaier.attr("class", ".powerImage1, .powerImage2, .powerImage3, .powerImage4"); 
+
+    // $("mariopowerupcontaier").append(powerImage1, powerImage2, powerImage3, powerImage4):
+
+}
+
+//game will have 4 power ups and random results
+//every powerup needs to have a random number asigned to it.
+// number needs to be generated every time player wins or losses to each power up
+// when clicking a power up it should add the total of all power ups clicked 
+    // untill player either wins or losses
+//then game starts all over
+//game score increments either a point to win or point to lose
