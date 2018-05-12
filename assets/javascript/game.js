924fcb1a1f7217c85fd2d1c33f838a6f2207ca1d
Wes Mora <wesmora@gmail.com>
2018-05-12 15:43:58 -0600

    update2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,62 @@
-var random_results;
-var losts;
-var wins;
+var randomNum =[];
+var losts = [];
+var wins = [];
+var starNum = [];
+var mushroomNum = [];
+var flowerNum = [];
+var starNum = [];
+var totalScore = 0;
 
-for(var i = 0; i < 4; i++){
-    var random = Math.floor (Math.random() * 12)
-        console.log (random)
-    // var mariopowerupcontai = $("<div>") 
-    // mariopowerupcontaier.attr("class", ".powerImage1, .powerImage2, .powerImage3, .powerImage4"); 
 
-    // $("mariopowerupcontaier").append(powerImage1, powerImage2, powerImage3, powerImage4):
 
+function ifElse() {
+    if (totalScore ===randomNum) {
+        wins++;
+        $("#wins").text("wins: " + wins);
+    } else if (totalScore > randomNum) {
+        losts
+    
 }
+
+
+$(document).ready(function(){
+
+    // function reset() {
+
+    randomNum =Math.floor((Math.random() * 100) + 20);
+    $("#randNum").text(randomNum);
+    console.log(randomNum);
+    starNum = Math.floor((Math.random() * 12) +1)
+    mushroomNum = Math.floor((Math.random() * 12) +1)
+    flowerNum = Math.floor((Math.random() *12) +1)
+    starNum = Math.floor((Math.random() * 12) +1)
+
+     {
+         randomNum =Math.floor((Math.random() * 100) + 20);
+        $("#randNum").text(randomNum);
+        console.log(randomNum);
+        starNum = Math.floor((Math.random() * 12) +1)
+        mushroomNum = Math.floor((Math.random() * 12) +1)
+        flowerNum = Math.floor((Math.random() *12) +1)
+        starNum = Math.floor((Math.random() * 12) +1)
+    }
+
+    
+
+
+
+
+});
+
+// for(var i = 0; i < 4; i++){
+//     var random = Math.floor (Math.random() * 12)
+//         console.log (random)
+//     var mariopowerupcontai = $("<div>") 
+//     mariopowerupcontaier.attr("class", ".powerImage1, .powerImage2, .powerImage3, .powerImage4"); 
+
+//     $("mariopowerupcontaier").append(powerImage1, powerImage2, powerImage3, powerImage4):
+
+
 
 //game will have 4 power ups and random results
 //every powerup needs to have a random number asigned to it.
